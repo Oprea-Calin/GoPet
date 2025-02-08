283da2af5e4cdc7236470126ad8c7295aa83dcad
Oprea Calin <128175751+Oprea-Calin@users.noreply.github.com>
2025-02-08 16:08:47 +0200

    clean build
    +Login/Register Views

--- app/src/main/java/com/example/gopet/MainActivity.java
+++ app/src/main/java/com/example/gopet/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gopet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editUsernameLog , editPasswordLog;
+    Button btnLogin, btnRegister;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        editUsernameLog = findViewById(R.id.inUsername);
+        editPasswordLog = findViewById(R.id.inPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view)
+            {
+                Intent i = new Intent(MainActivity.this, Register.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
 }
