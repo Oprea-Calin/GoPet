be875a59150d11ccf96ad99939c20535b89920a7
Oprea Calin <128175751+Oprea-Calin@users.noreply.github.com>
2025-04-14 17:50:23 +0300

    style, not so much

--- app/src/main/java/com/example/gopet/MainActivity.java
+++ app/src/main/java/com/example/gopet/MainActivity.java
@@ -170,6 +170,7 @@ public class MainActivity extends AppCompatActivity {
                 },
                 animal -> { // editListener
 
+                    usersRecyclerView.setVisibility(View.GONE);
                     if(profileShown == true){
                         profileRecycleView.setVisibility(View.GONE);
                         profileShown=false;
